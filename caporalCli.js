a10c0dafe2efb8dbc105eaf2244c23a734242ba1
Tabasqueau <mat78180@gmail.com>
2022-12-01 10:08:28 +0100

    Ajout du fichier gift parser et de la commande check dans le le caporalCli.js

--- caporalCli.js
+++ caporalCli.js
@@ -9,4 +9,30 @@ const cli = require("@caporal/core").default;
 cli
     .version('gift-parser-cli')
     .version('0.07')
-    // check Vpf
+    // check gift
+    .command('check', 'Check if <file> is a valid gift file')
+	.argument('<file>', 'The file to check with gift parser')
+	.option('-s, --showSymbols', 'log the analyzed symbol at each step', { validator : cli.BOOLEAN, default: false })
+	.option('-t, --showTokenize', 'log the tokenization results', { validator: cli.BOOLEAN, default: false })
+	.action(({args, options, logger}) => {
+		
+		fs.readFile(args.file, 'utf8', function (err,data) {
+			if (err) {
+				return logger.warn(err);
+			}
+	  
+			var analyzer = new VpfParser(options.showTokenize, options.showSymbols);
+			analyzer.parse(data);
+			
+			if(analyzer.errorCount === 0){
+				logger.info("The .gift file is a valid gift file".green);
+			}else{
+				logger.info("The .gift file contains error".red);
+			}
+			
+			logger.debug(analyzer.parsedPOI);
+
+		});
+			
+	})
+    cli.run(process.argv.slice(2));
